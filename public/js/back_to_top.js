32dce8ed09d7ce7ee9bb9330afc26f6beb0bfaae
Couret Alexandre <couretalexandredev@gmail.com>
2019-07-18 12:53:40 +0200

    Integration of a responsive navigation menu with display of a hamburger icon. It remains to establish all the responsive aspect of the home page, using breakpoints already established.

--- public/js/back_to_top.js
+++ public/js/back_to_top.js
@@ -1,4 +1,4 @@
-// Check distance to top and display back-to-top.
+// Vérifie la distance au sommet et affiche de haut en bas "back-to-top".
 $( window ).scroll( function() {
     if ( $( this ).scrollTop() > 800 ) {
         $( '.back-to-top' ).addClass( 'show-back-to-top' );
@@ -7,7 +7,7 @@ $( window ).scroll( function() {
     }
 });
 
-// Click event to scroll to top.
+// Clique sur l'événement pour faire défiler vers le haut
 $( '.back-to-top' ).click( function() {
     $( 'html, body' ).animate( { scrollTop : 0 }, 800 );
     return false;
