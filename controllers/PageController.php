53fad95aed91bfcc3370242c6ea798bdb7734e73
Couret Alexandre <couretalexandredev@gmail.com>
2019-07-28 13:12:03 +0200

    Deposit of a constructor in PageController, in order to make the car at Twig at any time the functions of the class. Management of errors in the router. Adding the path to Home in the header. Adding the path to my Linkedin profile in the footer. Image admin.png renamed login.png.

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -9,14 +9,19 @@ use Twig_Loader_Filesystem;
 
 class PageController
 {
-    public function index()
+    // Permet à toutes les fonctions de la classe d'accéder à Twig
+    public function __construct()
     {
         // Spécifie l'emplacement des templates Twig
         $this->loader = new Twig_Loader_Filesystem(__DIR__.'/../views');
 
         // Instancie Twig
         $this->twig = new Twig_Environment($this->loader);
+    }
 
+    // Affiche la page d'Accueil
+    public function index()
+    {
         echo $this->twig->render('index.html.twig' );
     }
 }
--- system/router.php
+++ system/router.php
@@ -8,10 +8,18 @@ use \Controllers\PageController;
 // Analyse l'URL récupèré par la superglobale $_SERVER et renvoie le chemin de l'URL analysée
 $path_only = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-if ($path_only == '/') {
-    $home = new PageController();
-    $home->index();
+// Gestion des erreurs
+try {
+    if ($path_only == '/') {
+        $home = new PageController();
+        $home->index();
+    }
+    else {
+        // Erreur gérée, elle sera remontée jusqu'au bloc try
+        throw new Exception('Page non trouvée.');
+    }
 }
-else {
-    echo 'Page non trouvée.';
+catch (Exception $e) {
+    // Affichage de l'erreur
+    echo 'Erreur : ' . $e->getMessage();
 }
