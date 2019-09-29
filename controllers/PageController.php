076468cb38a13acbf29312c6894ef4c50d638d2e
Couret Alexandre <couretalexandredev@gmail.com>
2019-09-29 19:58:59 +0200

    Added the Githun link in the footer. In the post model, change the  attribute to $ createDate. In the post model, Add attributes  and . Possibility now to modify the author of a post. Creating the posts.html.twig view that displays all posts. Creating the post.html.twig view that displays a post. Creating of the posts() method in PageController which displays all articles from the most recent to the oldest with a pagination. Creating the post() method in PageController that displays a page containing a single article. Created the gatAllPostsWithPaging() method in the Post model that retrieves all items with pagination.

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -4,11 +4,63 @@
 namespace Controllers;
 
 
+use Models\Post;
+
 class PageController extends Controller // Hérite de la class Controller et CheckFormValuesController
 {
     // Affiche la page d'Accueil
     public function index()
     {
         $this->render('index.html.twig', array());
+    }
+
+    // Affiche la page des articles avec une pagination
+    public function posts($page)
+    {
+        // Si la page éxiste
+        if ($page >= 1) {
+            // Définit le nombres d'articles par page
+            $nbPerPage = 3;
+
+            // Récupère tous les postes
+            $posts = Post::getAllPostsWithPaging($page, $nbPerPage);
+
+            // Calcule le nombre total de pages
+            $nbPages = ceil(count($posts)/$nbPerPage);
+
+            // Si la page éxiste
+            if ($page <= $nbPages) {
+                // Affiche la page des articles
+                $this->render('posts.html.twig', array(
+                    "posts" => $posts,
+                    "nbPages" => $nbPages,
+                    "page" => $page
+                ));
+            }
+            // Si la page n'éxiste pas
+            else {
+                // Redirection vers la 404
+                header("Location: /error404");
+                // Empêche l'exécution du reste du script
+                die();
+            }
+        }
+        // Si la page n'éxiste pas
+        else {
+            // Redirection vers la 404
+            header("Location: /error404");
+            // Empêche l'exécution du reste du script
+            die();
+        }
+    }
+
+    // Affiche un post
+    public function post($idPost)
+    {
+        // Récupère le post
+        $post = Post::getPost($idPost);
+
+        // Affiche la page de l'article
+        $this->render('post.html.twig', array("post" => $post));
     }
 }
